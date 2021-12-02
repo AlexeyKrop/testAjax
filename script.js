a46653d09b5d5095af8dca55ea9c9e9749da23e5
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2021-12-02 11:57:11 +0300

    add with axios

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 "use strcit";
 window.addEventListener("DOMContentLoaded", () => {
   const req = () => {
-    // let request = new XMLHttpRequest();
-    // request.open("GET", "http://localhost:3000/animals");
-    // request.send();
-    // request.addEventListener("load", () => {
-    //   if (request.readyState === 4 && request.status === 200) {
-    //     let data = JSON.parse(request.response);
-    //     getCards(data);
-    //   } else {
-    //     console.error("Что-то пошло не так");
-    //   }
-    // });
     getResourse("http://localhost:3000/animals")
-      .then((data) => getCards(data))
+      .then((data) => getCards(data.data))
       .catch((rer) => console.log("Ошибка"));
   };
   const getCards = (dataCards) => {
@@ -27,13 +16,12 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   };
   async function getResourse(url) {
-    let res = await fetch(url);
-    if (!res.ok) {
-      console.log("Ошибка");
-    }
-    return res.json();
+    let res = await axios(url);
+    // if (!res.ok) {
+    //   console.log("Ошибка");
+    // }
+    return res;
   }
-  getResourse("http://localhost:3000/animals");
   const btn = document.querySelector(".btn");
   btn.addEventListener("click", req);
 });
