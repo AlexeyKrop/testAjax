7438fb4bd0b4fe48e8ba4fa2ab1a85e3c46a7b26
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2021-12-05 11:18:48 +0300

    add new task

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 "use strcit";
-window.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector(".btn"),
-    form = document.querySelector("form");
-  const req = (e) => {
-    e.preventDefault();
-    let formData = new FormData(form);
-    formData.append("id", Math.random());
-    let obj = {};
-    formData.forEach((value, key) => {
-      obj[key] = value;
-    });
-    axios.post("http://localhost:3000/animals", obj);
-  };
-
-  form.addEventListener("submit", (e) => {
-    req(e);
-  });
-});
+const sendData = (data) => {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+const getData = () => {
+  fetch("db.json")
+    .then((responce) => responce.json())
+    .then((data) => sendData(data));
+};
+getData();
