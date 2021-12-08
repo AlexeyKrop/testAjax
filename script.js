7ceb0f00f068a92d40256f497fa5de3535266b11
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2021-12-08 14:41:46 +0300

    add new lesson

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 "use strcit";
-const sendData = (data) => {
-  fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+const sendForm = () => {
+  const form = document.querySelector(".form");
+  const sendData = (data) => {
+    return fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  };
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    sendData(formData).then((data) => {
+      console.log(data);
+    });
+  });
 };
-const getData = () => {
-  fetch("db.json")
-    .then((responce) => responce.json())
-    .then((data) => sendData(data));
-};
-getData();
+sendForm();
